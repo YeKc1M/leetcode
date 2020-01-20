9ed817fc9c5d4c97c9d69af969fc2110c7b34e56
YeKc1M <15008181891@163.com>
2020-01-20 12:02:05 +0800

    finish 3 sum. answer from leetcode

--- threeSum/Solution.java
+++ threeSum/Solution.java
@@ -1,31 +1,49 @@
-import java.util.HashSet;
-import java.util.LinkedList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
-    public List<List<Integer>> threeSum(int[] nums){
-        HashSet set=new HashSet();
+    public List<List<Integer>> threeSum(int[] nums){//copy from leetcode
+        if(nums.length<3) return new ArrayList<>();
+        Arrays.sort(nums);
+        List l=new ArrayList<>();
         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                for(int k=j+1;k<nums.length;k++){
-                    if(nums[i]+nums[j]+nums[k]==0){
-                        List add=new LinkedList();
-                        add.add(nums[i]);
-                        add.add(nums[j]);
-                        add.add(nums[k]);
-                        set.add(add);
-                    }
+            if(nums[i]>0) break;
+            if(i>0&&nums[i-1]==nums[i]) continue;
+            int left=i+1,right=nums.length-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if(sum==0){
+                    List temp=new ArrayList<>();
+                    temp.add(nums[i]);temp.add(nums[left]);temp.add(nums[right]);
+                    l.add(temp);
+                    while(left<right&&nums[left]==nums[left+1]) left++;
+                    while(left<right&&nums[right]==nums[right-1]) right--;
+                    left++;
+                    right--;
+                }else if(sum<0){
+                    left++;
+                }else{
+                    right--;
                 }
             }
         }
-        List l=new LinkedList<>(set);
         return l;
     }
     public static void main(String[] args){
         test();
+        //testSort();
     }
     public static void test(){
         int[] nums={-1,0,1,2,-1,-4};
         System.out.println(new Solution().threeSum(nums));
+    }
+    public static void testSort(){
+        int[] nums={-1,0,1,2,-1,-4};
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
     }
 }
