beaac4d115b38dfdc20f5a759eb977974cad4315
YeKc1M <15008181891@163.com>
2020-02-01 11:40:39 +0800

    add double-pointer solution

--- countAndSay/Solution.java
+++ countAndSay/Solution.java
@@ -29,12 +29,39 @@ public class Solution {
         }
         return s;
     }
+    //double pointer 18%-10%
+    public String countAndSay1(int n){
+        String s="1",temp="";
+        for(int i=0;i<n-1;i++){
+            int index1=0,index2=1,count=1;
+            while(index2<s.length()){
+                if(s.charAt(index1)==s.charAt(index2)){
+                    count++;
+                    index2++;
+                }else{
+                    temp+=(count+""+s.charAt(index1));
+                    index1=index2;index2++;count=1;
+                }
+            }
+            if(s.charAt(index2-1)==s.charAt(index1)){
+                temp+=(count+""+s.charAt(index1));
+            }else{
+                temp+=("1"+s.charAt(index2-1));
+            }
+            s=temp;temp="";
+        }
+        return s;
+    }
     public static void main(String[] args){
         System.out.println("count and say");
         //pass
         //for(int i=1;i<6;i++) test(i);
+        for(int i=1;i<6;i++) test1(i);
     }
     public static void test(int n){
         System.out.println(new Solution().countAndSay(n));
+    }
+    public static void test1(int n){
+        System.out.println(new Solution().countAndSay1(n));
     }
 }
