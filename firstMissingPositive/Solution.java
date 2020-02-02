e204bb6bca6a67c41f01c3fa777e7000252bb5b1
YeKc1M <15008181891@163.com>
2020-02-02 12:36:55 +0800

    add solution provided by leetcode

--- firstMissingPositive/Solution.java
+++ firstMissingPositive/Solution.java
@@ -12,38 +12,45 @@ public class Solution {
             if(!set.contains(i)) return i;
         }
     }
-    //unfinished
+    //by leetcode
     public int firstMissingPositive1(int[] nums) {
-        if(nums.length==0) return 1;
-        if(nums.length==1) return nums[0]==1? 2:1;
-        int min=0,min2=0;
-        int s=0;
-        //initilize min, min2
-        for(;s<nums.length;s++){
-            if(nums[s]>0){
-                min=nums[s];
-                s++;
+        boolean has1=false;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                has1=true;
                 break;
             }
         }
-        if(min==0) return 1;
-        for(;s<nums.length;s++){
-            if(nums[s]>0){
-                min2=nums[s];
-                s++;
-                break;
+        if(!has1) return 1;
+        if(nums.length==1) return 2;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<=0||nums[i]>nums.length) nums[i]=1;
+        }
+        for(int i=0;i<nums.length;i++){
+            int a=Math.abs(nums[i]);
+            if(a==nums.length){
+                nums[0]=-Math.abs(nums[0]);
+            }else{
+                nums[a]=-Math.abs(nums[a]);
             }
         }
-        if(min2==0) return min==1? 2:1;
-        Set set=new HashSet<>();
-        return 0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>0) return i;
+        }
+        if(nums[0]>0) return nums.length;
+        return nums.length+1;
     }
     public static void main(String[] args){
         System.out.println("first missing positive");
-        test1();
+        //test1();
+        test2();
     }
     public static void test1(){
         int[] nums={7,8,9,11,12};
         System.out.println(new Solution().firstMissingPositive(nums));
+    }
+    public static void test2(){
+        int[] nums={1,2,0};
+        System.out.println(new Solution().firstMissingPositive1(nums));
     }
 }
