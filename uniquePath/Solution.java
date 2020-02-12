c8ab538d72fdbf43ec4f6a5bbd1fad5963b9ae96
YeKc1M <15008181891@163.com>
2020-02-12 11:32:30 +0800

    add dynamic programming

--- uniquePath/Solution.java
+++ uniquePath/Solution.java
@@ -9,11 +9,29 @@ public class Solution {
         for(int i=1;i<=b;i++) res/=i;
         return (int)res;
     }
+    //dynamic programming 67% 14%
+    public int uniquePaths1(int m, int n){
+        if(m==1||n==1) return 1;
+        int[][] matrix=new int[m][n];
+        matrix[0][0]=0;
+        for(int i=1;i<m;i++) matrix[i][0]=1;
+        for(int i=1;i<n;i++) matrix[0][i]=1;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                matrix[i][j]=matrix[i-1][j]+matrix[i][j-1];
+            }
+        }
+        return matrix[m-1][n-1];
+    }
     public static void main(String[] args){
         System.out.println("unique path");
-        test();
+        //test();
+        test1();
     }
     public static void test(){
         System.out.println(new Solution().uniquePaths(10, 10));
+    }
+    public static void test1(){
+        System.out.println(new Solution().uniquePaths1(10, 10));
     }
 }
