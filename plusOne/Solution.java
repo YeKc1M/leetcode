443fd0059fc86fe33c9d88e7639a1b76974183e6
YeKc1M <15008181891@163.com>
2020-02-13 10:33:52 +0800

    add normal solution

---
+++ plusOne/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class Solution {
+    //normal solution 100%-5%
+    public int[] plusOne(int[] digits) {
+        int[] res=new int[digits.length+1];
+        res[0]=0;
+        int flag=1;
+        for(int i=res.length-1;i>0;i--){
+            int j=digits[i-1]+flag;
+            res[i]=j%10;
+            flag=j/10;
+        }
+        res[0]+=flag;
+        if(res[0]==0) res=Arrays.copyOfRange(res, 1, res.length);
+        return res;
+    }
+    public static void main(String[] args){
+        System.out.println("plus one");
+        test();
+    }
+    private static void test(){
+        int[] digits={4,3,2,1};
+        System.out.println(new Solution().plusOne(digits));
+    }
+}
