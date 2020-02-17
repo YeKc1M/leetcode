b7f6e3e087df281d9ceb5dff4dc17a1e42cd9fea
YeKc1M <15008181891@163.com>
2020-02-17 11:27:24 +0800

    add solution

--- sortColor/Solution.java
+++ sortColor/Solution.java
@@ -13,11 +13,36 @@ public class Solution {
             }
         }
     }
+    //100% 5%
+    public void sortColors1(int[] nums){
+        int cursor=0,index0=0,index2=nums.length-1;
+        while(index0<=index2&&nums[index0]==0){
+            index0++;
+            cursor++;
+        }
+        while(index0<=index2&&nums[index2]==2) index2--;
+        while(cursor<=index2){
+            if(nums[cursor]==0){
+                nums[cursor]=nums[index0];
+                nums[index0++]=0;
+            }else if(nums[cursor]==2){
+                nums[cursor]=nums[index2];
+                nums[index2--]=2;
+                if(nums[cursor]==0){
+                    nums[cursor]=nums[index0];
+                    nums[index0++]=0;
+                }
+                while(index0<=index2&&nums[index2]==2) index2--;
+            }
+            cursor++;
+        }
+    }
     public static void main(String[] args){
         System.out.println("sort color");
+        test();
     }
     private static void test(){
-        int[] nums={1};
-        //
+        int[] nums={0,2,2,2,0,2,1,1};
+        new Solution().sortColors1(nums);
     }
 }
