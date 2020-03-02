9a454712f2571f66de7a5297a8af735dd9488b08
YeKc1M <15008181891@163.com>
2020-03-02 08:46:55 +0800

    add solution

---
+++ binaryTreeFromSortedArr/TreeNode.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        TreeNode root=null;
+        if(nums.length!=0){
+            if(nums.length==1){
+                root=new TreeNode(nums[0]);
+            }else{
+                int n=nums.length;
+                root=new TreeNode(nums[n/2]);
+                root.left=sortedArrayToBST(Arrays.copyOfRange(nums, 0, n/2));
+                root.right=sortedArrayToBST(Arrays.copyOfRange(nums, n/2+1, n));
+            }
+        }
+        return root;
+    }
+}
