94d77427ab609f70f2a4c1f820eac52287188537
YeKc1M <383250208@qq.com>
2020-11-15 11:31:14 +0800

    removekdigits solution 2: package error unresolved

--- removekdigits/RemoveKDigits.java
+++ removekdigits/RemoveKDigits.java
@@ -3,8 +3,12 @@ public class RemoveKDigits {
         System.out.println("hello world!");
         System.out.println(new RemoveKDigits()
             .removeKdigits("112", 1));
+        System.out.println(new RemoveKDigits()
+        .removeKDigits_II("5337", 2));
     }
-
+    /*
+     * my solution 1: remove one by one
+    */
     public String removeKdigits(String num, int k) {
         StringBuilder builder=new StringBuilder(removeOneDigit(num, 0, k));
         return builder.toString();
@@ -26,5 +30,41 @@ public class RemoveKDigits {
             builder.replace(0, 1, "");
         }
         return removeOneDigit(builder.toString(), depth+1, k);
+    }
+    /*
+     * my solution 2
+     */
+    public String removeKDigits_II(String num, int k){
+        if(num.length()<=k) return "0";
+        StringBuilder builder=new StringBuilder();
+        int count=0;
+        int index=1;
+        builder.append(num.charAt(0));
+        char current;
+        while(count<k&&index<num.length()){
+            current=num.charAt(index++);
+            if(builder.charAt(builder.length()-1)>current){
+                do{
+                    builder.deleteCharAt(builder.length()-1);
+                    count++;
+                }while(count<k&&
+                builder.length()>0&&
+                builder.charAt(builder.length()-1)>current);
+            }
+            builder.append(current);
+        }
+        while(count<k){
+            builder.deleteCharAt(builder.length()-1);
+            count++;
+        }
+        while(index<num.length()){
+            for(;index<num.length();index++){
+                builder.append(num.charAt(index));
+            }
+        }
+        while(builder.length()>1&&builder.charAt(0)=='0'){
+            builder.deleteCharAt(0);
+        }
+        return builder.toString();
     }
 }
