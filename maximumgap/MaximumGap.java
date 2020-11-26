7d9d385e59bc05ae5e96b1d4931788f9664e6554
kangyao.ma@sap.com <kangyao.ma@sap.com>
2020-11-26 14:48:09 +0800

    maximumgap: add normal solution

--- maximumgap/MaximumGap.java
+++ maximumgap/MaximumGap.java
@@ -10,10 +10,22 @@ public class MaximumGap {
 }
 
 class Solution {
+    //
     public int maximumGap(int[] nums) {
         int res=0;
         if(nums.length<2) return 0;
         //
+        return res;
+    }
+    //normal solution
+    //pass
+    public int maximumGap1(int[] nums) {
+        int res=0;
+        if(nums.length<2) return 0;
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 1; i++){
+            res=Math.max(res, nums[i + 1] - nums[i]);
+        }
         return res;
     }
 }
