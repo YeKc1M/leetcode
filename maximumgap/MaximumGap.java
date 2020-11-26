8c9d69209a36959057c6e95c27bb6e6a6f439ba8
kangyao.ma@sap.com <kangyao.ma@sap.com>
2020-11-26 11:49:13 +0800

    maximumgap unfinished

---
+++ maximumgap/MaximumGap.java
@@ -0,0 +1,19 @@
+package maximumgap;
+
+import java.util.Arrays;
+
+//https://leetcode-cn.com/problems/maximum-gap/
+public class MaximumGap {
+    public static void main(String[] args){
+        System.out.println("x");
+    }
+}
+
+class Solution {
+    public int maximumGap(int[] nums) {
+        int res=0;
+        if(nums.length<2) return 0;
+        //
+        return res;
+    }
+}
